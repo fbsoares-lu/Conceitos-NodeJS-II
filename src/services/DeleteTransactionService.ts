9be5818ea015e402e600659ebc3d769cff5ca9c0
Lucas Soares <lsoareshfb@gmail.com>
2020-05-21 01:57:09 -0300

    Create CreateTransaction and DeleteTransaction

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,24 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import Transaction from '../models/Transaction';
+
+interface Request {
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: Request): Promise<void> {
+    const transacitonRepository = getRepository(Transaction);
+
+    const transactionExists = await transacitonRepository.findOne({
+      where: { id },
+    });
+
+    if (!transactionExists) {
+      throw new AppError('Transaction is not found', 401);
+    }
+
+    await transacitonRepository.delete(id);
   }
 }
 
