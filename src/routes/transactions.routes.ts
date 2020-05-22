3dede43201da4b8eb87b05d0badfef65b50d3a81
Lucas Soares <lsoareshfb@gmail.com>
2020-05-22 16:11:18 -0300

    Adicção da class category

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { getRepository } from 'typeorm';
+import Transaction from '../models/Transaction';
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
@@ -8,7 +10,11 @@ import DeleteTransactionService from '../services/DeleteTransactionService';
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const transactionRepository = getRepository(Transaction);
+
+  const transaction = await transactionRepository.find();
+
+  return response.json(transaction);
 });
 
 transactionsRouter.post('/', async (request, response) => {
